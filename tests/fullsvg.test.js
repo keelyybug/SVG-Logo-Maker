649b38358b10475698d8afdbe32e97883fa01b85
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-04-25 15:49:19 -0400

    started to debug

--- tests/fullsvg.test.js
+++ tests/fullsvg.test.js
@@ -2,59 +2,63 @@ const Triangle = require('../lib/triangle');
 const Square = require('../lib/square');
 const Circle = require('../lib/circle');
 
+
+
 describe("Circle", () => {
-    it("should take three initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
+    it("should take three input initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
 
         const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-        <circle cx="150" cy="100" r="80" fill="pink" />
+        <circle cx="150" cy="100" r="80" fill="pink"/>
       
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">LOL</text>
       
       </svg>`
 
       const [text, textColor, shapeColor] = ["LOL", "white", "pink"];
-      const circle = new Circle(text, textColor, shapeColor);
-      const actualSvg = circle.render()
-      expectedSvg(actualSvg).toEqual(expectedSvg);
+      const circle = new Circle(shapeColor, text, textColor);
+      const actualSvg = circle.render();
+      console.log(actualSvg);
+      expect(actualSvg).toEqual(expectedSvg);
 });
 })
 
 describe("Square", () => {
-    it("should take three initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
+    it("should take three input initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
 
         const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-        <rect x="90" y="40" width="120" height="120" fill="pink" />
+        <rect x="90" y="40" width="120" height="120" fill="pink"/>
       
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">LOL</text>
       
       </svg>`
 
       const [text, textColor, shapeColor] = ["LOL", "white", "pink"];
-      const square = new Square(text, textColor, shapeColor);
-      const actualSvg = square.render()
-      expectedSvg(actualSvg).toEqual(expectedSvg);
+      const square = new Square(shapeColor, text, textColor);
+      const actualSvg = square.render();
+      expect(actualSvg).toEqual(expectedSvg);
 });
 })
 
 describe("Triangle", () => {
-        it("should take three initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
+        it("should take three input initials and fill them with the input color, and do the same with the input shape and the input shape color", () => {
 
             const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-            <polygon points="150, 18 244, 182 56, 182" fill="pink" />
+            <polygon points="150, 18 244, 182 56, 182" fill="pink"/>
           
             <text x="150" y="150" font-size="60" text-anchor="middle" fill="white">LOL</text>
           
             </svg>`
 
           const [text, textColor, shapeColor] = ["LOL", "white", "pink"];
-          const triangle = new Triangle(text, textColor, shapeColor);
-          const actualSvg = triangle.render()
-          expectedSvg(actualSvg).toEqual(expectedSvg);
+          const triangle = new Triangle(shapeColor, text, textColor);
+          const actualSvg = triangle.render();
+          expect(actualSvg).toEqual(expectedSvg);
     });
 })
+
 //test('should render green triangle', () => { 
     //const expectedSvg = `<polygon points="150, 18 244, 182 56, 182" fill="green" />`;
     //const triangle = new Triangle();
